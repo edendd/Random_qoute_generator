331edf48bbc9d8b3e76b4d25210b75c0c5a9ed6c
edenhailu333@gmail.com <EDENhailu21>
2021-04-25 10:43:42 -0500

    printout function fix

--- js/script.js
+++ js/script.js
@@ -13,35 +13,40 @@ project 1 - A Random Quote Generator
 // Quotes array with some of my favorite lines.
 
 const quotes = [
-{ 
-qoute:' "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." ',
-source: 'Patrick McKenzie ',
-year: 2016,
-citation: 'Twitter'
-},
+  {
+  quote: 'It is better to be hated for what you are than to be loved for what you are not.',
+  source: 'Andre Gide', 
+  citation: 'Autumn Leaves',
+  year: 'unknown'
+  },
+   
+  {
 
-{
- qoute: '"It is better to be hated for what you are than to be loved for what you are not."',
- source: 'Andre Gide', 
- citation: 'Autumn Leaves'
-},
- 
-{
- qoute:' " You only live once, but if you do it right, once is enough Mae West"',
+  quote: ' You only live once, but if you do it right, once is enough Mae West',
   source: 'Mahatma Gandh'
-},
-
-{
- qoute:' "Be the change that you wish to see in the world."',
- source: 'Mahatma Gandh'
-},
-
-{
-  qoute:' "I have learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."',
-  source: 'Maya Angelou'
-}
   
-]
+  },
+  
+  {
+
+  quote: 'Be the change that you wish to see in the world.',
+  source: 'Mahatma Gandh'
+  
+  },
+  
+  {
+  quote: 'I have learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.',
+  source: 'Maya Angelou'
+  },
+  
+  {
+  quote: 'Any application that can be written in JavaScript, will eventually be written in JavaScript',
+  source: 'Jeff Atwood',
+  citation: 'codinghorror',
+  year: 2007 
+  }
+  
+  ]
   
   
   /***
@@ -50,8 +55,10 @@ citation: 'Twitter'
   // function to choose a random number and then pull the corresponding quote.
   
   function getRandomQuote() {
-    let randomNum =  Math.floor( Math.random() * quotes.length )
-    return quotes[randomNum];
+
+    let randNumber = Math.floor(Math.random() * quotes.length);
+
+    return quotes[randNumber];
   }
   //checking random quote function
   // console.log(getRandomQuote() );
@@ -62,27 +69,23 @@ citation: 'Twitter'
   ***/
   // function that uses the random object and prints to html.
   
-  function printQuote () {
-
-    let Rquotes = getRandomQuote();
-
-  let quoteinfo= `<P class = "qoute"> ${Rquotes.qoute} </p><p class = "source"> ${Rquotes.source};`
-       
-  if (Rquotes.citation) {
-    quoteinfo += `<span class="citation"> ${Rquotes.citation}  </span>`;
+  function printQuote() {
+    
+    let RQuote = getRandomQuote();
+    
+    let qouteEle = `<p class= "quote" >${RQuote.quote}</p><p class= "source" >${RQuote.source} `;
+      
+      if  (RQuote.citation) {
+        qouteEle += `<span class = "citation">${RQuote.citation}</span>`;
+      }
+      
+      if (RQuote.year) {
+        qouteEle += `<span class="year">${RQuote.year}</span>`;
+      }
+    let htmlFinal = qouteEle + `</p>`;
+    
+  document.getElementById('quote-box').innerHTML = htmlFinal;
   }
-  
-  if(Rquotes.year){
-    quoteinfo += `<span class='year'> ${Rquotes.year} </span>`;
-  }
-  
-  let lastPrint = quoteinfo + `</p>`;
-  
-  document.getElementById('quote-box').innerHTML =  lastPrint; 
- 
-
-}
-
   // console.log(printQuote);
   
   /***
